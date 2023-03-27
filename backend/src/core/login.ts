d5bc033b76d0040c692ff8aa9b1065d18fc14746
Ruben Herrera <rubenherrera@wetaca.com>
2023-03-27 11:56:12 +0200

    comments add

--- backend/src/core/login.ts
+++ backend/src/core/login.ts
@@ -7,6 +7,7 @@ const prisma = new PrismaClient()
 
 export const login = async (loginInput: LoginInput) => {
   try {
+    // Find a user with the specified email
     const usuario = await prisma.usuario.findUnique({
       where: {
         email: loginInput.email,
@@ -15,6 +16,7 @@ export const login = async (loginInput: LoginInput) => {
 
     if (!usuario) return null
 
+    // Check if the password provided by the user matches the hashed password in the database
     const validPassword = await bcrypt.compare(
       loginInput.password,
       usuario.password
@@ -22,6 +24,7 @@ export const login = async (loginInput: LoginInput) => {
 
     if (!validPassword) return null
 
+    // If the password is valid, generate a JWT token
     const token = jwt.sign(
       {
         email: usuario.email,
--- backend/src/middleware/validate-token.ts
+++ backend/src/middleware/validate-token.ts
@@ -3,13 +3,20 @@ import { jwt } from "jsonwebtoken"
 export const verifyToken = (req, res, next) => {
     const token = req.header('token')
 
+    // Check if token exists
     if(!token) return res.status(401).json({error: 'Acceso denegado'})
 
     try {
+        // Verify token using the provided secret key
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+
+         // Store user data in the request object
         req.user = verified
+
+        // Move to the next middleware
         next()
     } catch (error){
+        // Handle errors related to token verification
         res.status(400).json({error: 'Token no valido, acceso denegado'})
     }
 }
