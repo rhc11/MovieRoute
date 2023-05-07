9ca706a3829b27a3a1c31f7f921e2c410f6e20f3
Ruben Herrera <rubenherrera@wetaca.com>
2023-05-07 12:38:17 +0200

    scroll view

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,6 +1,7 @@
 import { Icon, TabBar } from "@ant-design/react-native"
 import { useState } from "react"
 import { useNavigate } from "react-router-native"
+import { View } from "react-native"
 
 export const Menu: React.FC = () => {
   const navigate = useNavigate()
@@ -12,31 +13,33 @@ export const Menu: React.FC = () => {
   }
 
   return (
-    <TabBar unselectedTintColor="#949494" tintColor="#ffcd6b">
-      <TabBar.Item
-        title="Home"
-        icon={<Icon name="home" />}
-        selected={selected === "/home"}
-        onPress={() => onChangeTab("/home")}
-      />
-      <TabBar.Item
-        title="Mis rutas"
-        icon={<Icon name="video-camera" />}
-        selected={selected === "/misRutas"}
-        onPress={() => onChangeTab("/misRutas")}
-      />
-      <TabBar.Item
-        title="Favoritos"
-        icon={<Icon name="star" />}
-        selected={selected === "/favorites"}
-        onPress={() => onChangeTab("/favorites")}
-      />
-      <TabBar.Item
-        title="Perfil"
-        icon={<Icon name="user" />}
-        selected={selected === "/user"}
-        onPress={() => onChangeTab("/user")}
-      />
-    </TabBar>
+    <View style={{ zIndex: 2 }}>
+      <TabBar unselectedTintColor="#949494" tintColor="#ffcd6b">
+        <TabBar.Item
+          title="Home"
+          icon={<Icon name="home" />}
+          selected={selected === "/home"}
+          onPress={() => onChangeTab("/home")}
+        />
+        <TabBar.Item
+          title="Mis rutas"
+          icon={<Icon name="video-camera" />}
+          selected={selected === "/misRutas"}
+          onPress={() => onChangeTab("/misRutas")}
+        />
+        <TabBar.Item
+          title="Favoritos"
+          icon={<Icon name="star" />}
+          selected={selected === "/favorites"}
+          onPress={() => onChangeTab("/favorites")}
+        />
+        <TabBar.Item
+          title="Perfil"
+          icon={<Icon name="user" />}
+          selected={selected === "/user"}
+          onPress={() => onChangeTab("/user")}
+        />
+      </TabBar>
+    </View>
   )
 }
--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,14 +1,26 @@
+import { ScrollView, Text } from "react-native"
 import { Menu } from "../components/menu"
 import { tw } from "../lib/tailwind"
 import { SearchBar, View } from "@ant-design/react-native"
 
 export const Home = () => {
   return (
-    <View>
-      <SearchBar
-        placeholder="Buscar ruta"
-        style={tw`fabsolute inset-x-0 top-0`}
-      />
+    <View style={{ flex: 1, zIndex: 1 }}>
+      <SearchBar placeholder="Buscar ruta" style={tw`inset-x-0 top-0`} />
+
+      <ScrollView style={{ flex: 1, zIndex: 0 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 20,
+          }}
+        >
+          <Text>Contenido del ScrollView</Text>
+        </View>
+      </ScrollView>
+
       <Menu />
     </View>
   )
